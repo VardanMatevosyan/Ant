e4352478e6ce6e2f6c3b2c19a8cc9d9d20e3adfc
VardanMatevosyan <vardya@yandex.ru>
2019-03-24 14:30:28 +0200

    add ant simple project

---
+++ simple/src/main/java/ru/matevosyan/Main.java
@@ -0,0 +1,9 @@
+package ru.matevosyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Ant!");
+    }
+}
